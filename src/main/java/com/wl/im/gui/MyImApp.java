ae53e1f496ce9dc1fe88e479a70c95de0efb7c7e
wanglei <wanglei@saicmobility.com>
2020-11-24 18:43:42 +0800

    简易聊天室开发

---
+++ src/main/java/com/wl/im/gui/MyImApp.java
@@ -0,0 +1,29 @@
+package com.wl.im.gui;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+/**
+ * Created on 2020/11/23 13:57
+ *
+ * @author wanglei
+ * @Description
+ * @projectName wl-IM
+ */
+public class MyImApp extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        BorderPane root = new BorderPane();
+        Scene scene = new Scene(root, 400, 400);
+        scene.getStylesheets().add(getClass().getResource("/MyImApp.css").toExternalForm());
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
